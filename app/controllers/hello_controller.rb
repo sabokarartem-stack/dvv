16d27e997dddac1a19716f40ec895757d1f7c992
sabokarartem-stack <sabokarartem@gmail.com>
2026-05-20 19:45:01 +0200

    Add CD to AWS Elastic Beanstalk

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,9 +2,13 @@
 
 class HelloController < ApplicationController
   def index
+    # Читаємо секрет із захищених змінних середовища AWS
+    secret_key = ENV['API_SECRET_KEY'] || 'Секрет не знайдено!'
+
     render json: {
       status: 'success',
-      message: 'Hello World from Ruby on Rails!'
+      message: 'Hello from Ruby on Rails on AWS Elastic Beanstalk!',
+      secret: secret_key
     }
   end
 end
