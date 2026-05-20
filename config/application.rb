f039f099dc150d0774a24b757298284ec52b7457
sabokarartem-stack <sabokarartem@gmail.com>
2026-05-20 22:58:06 +0200

    Allow AWS Elastic Beanstalk hosts

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module DevopsProject
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Дозволяємо доступ з доменів AWS Elastic Beanstalk
+    config.hosts << /.*\.elasticbeanstalk\.com/
   end
 end
